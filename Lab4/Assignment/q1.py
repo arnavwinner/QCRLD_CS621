701a57b5cad84296740c43db9e7e3234ac585cb3
arnavwinner <gautam.arnavtokyo@gmail.com>
2024-02-01 23:52:42 +0530

    Added Lab4

--- Lab4/Assignment/q1.py
+++ Lab4/Assignment/q1.py
@@ -4,19 +4,25 @@ from qiskit.tools.visualization import circuit_drawer
 M_simulator = Aer.backends(name='qasm_simulator')[0]
 S_simulator = Aer.backends(name ='statevector_simulator')[0]
 
-q = QuantumRegister(3)
-c = ClassicalRegister(3)
+q = QuantumRegister(2)
+c = ClassicalRegister(2)
 qc = QuantumCircuit(q, c)
+
 qc.h(q[0])
-qc.x(q[1])
-qc.ccx(q[0], q[1], q[2])
+qc.id(q[1])
+
 S = execute(qc, S_simulator).result().get_statevector()
 print(S)
+
+qc.cx(q[0], q[1])
+
+S = execute(qc, S_simulator).result().get_statevector()
+print(S)
+print("------------------------------------------")
 qc.measure(q, c)
+
 M = execute(qc, M_simulator, shots=100).result().get_counts(qc)
-print(M)
-S = execute(qc, S_simulator).result().get_statevector()
-print(S)
+print("After Measurement: ", M)
 print(qc.draw())
 
 
--- Lab4/Assignment/q4.py
+++ Lab4/Assignment/q4.py
@@ -1,3 +1,5 @@
+#AND GATE USING CCNOT
+
 from qiskit import QuantumRegister, QuantumCircuit, Aer, execute
 from qiskit import ClassicalRegister
 from qiskit.tools.visualization import circuit_drawer
@@ -7,18 +9,14 @@ S_simulator = Aer.backends(name ='statevector_simulator')[0]
 q = QuantumRegister(3)
 c = ClassicalRegister(3)
 qc = QuantumCircuit(q, c)
-qc.h(q[0])
-qc.x(q[1])
-qc.ccx(q[0], q[1], q[2])
-S = execute(qc, S_simulator).result().get_statevector()
-print(S)
+qc.h(q[0]) #x = 1
+qc.h(q[1]) #y = 1/0
+qc.id(q[2]) #z = 0 -- z ^ (x & y)
+qc.ccx(q[0], q[1], q[2]) # final value is y
 qc.measure(q, c)
+print(qc.draw())
 M = execute(qc, M_simulator, shots=100).result().get_counts(qc)
 print(M)
-S = execute(qc, S_simulator).result().get_statevector()
-print(S)
-print(qc.draw())
-
 
 # print(circuit_drawer(qc))
 # S = execute(qc, S_simulator).result().get_statevector()
