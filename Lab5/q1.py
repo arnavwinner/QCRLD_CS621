3e2e22ed523c9eda3a2d2b61329792ee5e765be1
arnavwinner <gautam.arnavtokyo@gmail.com>
2024-02-08 09:27:19 +0530

    Started Lab5 q1 done

---
+++ Lab5/q1.py
@@ -0,0 +1,24 @@
+from qiskit import QuantumRegister, QuantumCircuit, Aer, execute
+from qiskit import ClassicalRegister
+from qiskit.tools.visualization import circuit_drawer
+M_simulator = Aer.backends(name='qasm_simulator')[0]
+S_simulator = Aer.backends(name ='statevector_simulator')[0]
+
+q = QuantumRegister(2)
+c = ClassicalRegister(2)
+qc = QuantumCircuit(q, c)
+qc.h(q[0])
+qc.cx(q[0], q[1])
+S = execute(qc, S_simulator).result().get_statevector()
+print(S)
+qc.measure(q, c)
+M = execute(qc, M_simulator, shots=100).result().get_counts(qc)
+print(M)
+# S = execute(qc, S_simulator).result().get_statevector()
+# print(S)
+print(qc.draw())
+
+
+# print(circuit_drawer(qc))
+# S = execute(qc, S_simulator).result().get_statevector()
+# print(S)
